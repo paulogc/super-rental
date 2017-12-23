92b84b88ead9a9c6fbd81b14204691956c1e27d0
Guedes <paulo.guedes@vistajet.com>
2017-12-23 10:36:04 -0200

    setting api mirage

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -2,27 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [
-      {
-        id: 'grand-old-mansion',
-        title: 'Grand Old Mansion',
-        owner: 'Veruca Salt',
-        city: 'San Francisco',
-        category: 'Estate',
-        bedrooms: 15,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-        description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-      },
-      {
-        id: 'urban-living',
-        title: 'Urban Living',
-        owner: 'Mike TV',
-        city: 'Seattle',
-        category: 'Condo',
-        bedrooms: 1,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
-        description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
-      }
-    ];
+    return this.get('store').findAll('rental');
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -5,25 +5,29 @@ export default function() {
     {
       data:  [
         {
+          type: 'rentals',
           id: 'grand-old-mansion',
-          title: 'Grand Old Mansion',
-          owner: 'Veruca Salt',
-          city: 'San Francisco',
-          category: 'Estate',
-          bedrooms: 15,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-          description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-        },
+          attributes: {
+            title: 'Grand Old Mansion',
+            owner: 'Veruca Salt',
+            city: 'San Francisco',
+            category: 'Estate',
+            bedrooms: 15,
+            image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+          }
+        }, 
         {
+          type: 'rentals',
           id: 'urban-living',
-          title: 'Urban Living',
-          owner: 'Mike TV',
-          city: 'Seattle',
-          category: 'Condo',
-          bedrooms: 1,
-          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
-          description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
-        }
+          attributes: {
+            title: 'Urban Living',
+            owner: 'Mike Teavee',
+            city: 'Seattle',
+            category: 'Condo',
+            bedrooms: 1,
+            image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+          }
+        },
       ],
     })
   );
